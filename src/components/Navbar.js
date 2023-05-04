b5b3f660a5d8dcd585071fc012a486dafa407559
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-05-04 15:01:25 +0530

    navbar component added

---
+++ src/components/Navbar.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { Mail, Notifications, PersonAddAlt1 } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+  Switch
+} from "@mui/material";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 15px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
+
+const Navbar = ({mode,setMode}) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          MUI KICKSTART
+        </Typography>
+        <Search>
+          <InputBase placeholder="Search here..." />
+        </Search>
+        <Icons>
+         <Badge badgeContent={5} color="error">
+            <PersonAddAlt1 />
+          </Badge>
+          <Badge badgeContent={7} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={1} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://material-ui.com/static/images/avatar/6.jpg"
+            onClick={(e) => setOpen(true)}
+          />
+        </Icons>
+        <UserBox>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://material-ui.com/static/images/avatar/6.jpg"
+            onClick={(e) => setOpen(true)}
+          />
+          <Typography fontSize={"12px"}>
+          {mode === "light" ? "Light Mode" : "Dark Mode"}
+        <Switch onChange={e=>setMode(mode === "light" ? "dark" : "light")}/>
+        </Typography>
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>My Profile</MenuItem>
+        <MenuItem>My Account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
+};
+
+export default Navbar;
